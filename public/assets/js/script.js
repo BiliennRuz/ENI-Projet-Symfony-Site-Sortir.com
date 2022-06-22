7be999feb6af56295cde0d7d219dacb83bdc6906
Antoine1989 <antoine-guillou@orange.fr>
2022-06-22 16:41:23 +0200

    ADD: annulation de sortie ok

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -5,44 +5,36 @@ item.addEventListener("mouseover", show2,false);*/
 
 console.log("yo");
 
-/*function show(item)
-{  
-    //let item = document.getElementById("buttonShow");
-   //document.getElementById("buttonShow").className="action-button primary";
-   console.log("survol ok");
-    item = document.getElementsByClassName("action-button second primary");
-   item.replace,"action-button second primary";
- 
-   return item;
-}
-*/
+
 //Bouton voir une sortie/////////////////////////
-//let item2=document.getElementsByClassName("action-button second link");
+let item2=document.getElementsByClassName("action-button second link");
 
-function show2(){
-
-    console.log("survol2 ok");
-    let item2 = document.getElementsByClassName("action-button second link")
-//for (var i = 0; i < item2.length; i++) {
-  //if (item2[i].getAttribute("class") === "action-button second link"){
-   item2.setAttribute("class", "action-button second primary");
+function show2(item2){
+   
+    console.log("survol0 ok");
+    item2 = document.getElementsByClassName("action-button second link");
+for (var i = 0; i < item2.length; i++) {
+if (item2[i].getAttribute("class") === "action-button second link"){
+   item2[i].setAttribute("class", "action-button second primary");
 }
-//return item2;
-  //}
-//}
+return item2;
+  }
+}
 
-//let item3=document.getElementsByClassName("action-button second primary");
+let item3=document.getElementsByClassName("action-button second primary");
+//item3.addEventListener("mouseover", out,false);
 
-function out(){ 
-    console.log("survol2 ok");
-     let item3 = document.getElementsByClassName("action-button second primary")
+function out(item3){ 
+    console.log("survol1 ok");
+      item3 = document.getElementsByClassName("action-button second primary");
 for (var i = 0; i < item3.length; i++) {
   if (item3[i].getAttribute("class") === "action-button second primary"){
    item3[i].setAttribute("class", "action-button second link");
 }
-//return item3;
+return item3;
   }
 }
+
 
 //Bouton créer une sortie/////////////////////////
 
